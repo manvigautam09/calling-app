c7a99d21f8ac2f6def3c2cf5ccf121dce3eb2ccd
manvigautam09 <mnvsgautam9@gmail.com>
2021-02-23 22:01:04 +0530

    added fallback route

--- src/router/index.js
+++ src/router/index.js
@@ -2,10 +2,13 @@ import { createWebHistory, createRouter } from "vue-router";
 
 import Home from "../Views/Home";
 import Room from "../Views/Room";
+import PageNotFound from "../Views/PageNotFound";
 
 const routes = [
   { path: "/", name: "Home", component: Home },
   { path: "/room/:roomName/:id", name: "Room", component: Room },
+  { path: "/:pathMatch(.*)", component: PageNotFound },
+  { path: "/:pathMatch(.*)*", component: PageNotFound },
 ];
 
 const router = createRouter({
