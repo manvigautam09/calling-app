2fb6c733fb401b71031b7ba58531d5e954978d21
Manvi Sharma <42731523+manvigautam09@users.noreply.github.com>
2021-09-07 15:09:21 +0530

    css fixes

--- src/App.vue
+++ src/App.vue
@@ -8,4 +8,10 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+body {
+  margin-top: -8px !important;
+  margin-left: -4px !important;
+  margin-right: -4px !important;
+}
+</style>
--- src/Views/Login/index.vue
+++ src/Views/Login/index.vue
@@ -26,25 +26,6 @@
       <p className="login-signUp-link">
         <router-link to="/signup">Register</router-link>
       </p>
-
-      <!--       
-      <p className="forgot-password">
-        <router-link to="/forgot-password">Forgot password ?</router-link>
-      </p> -->
-
-      <!-- <div className="social-icons">
-        <ul>
-          <li>
-            <a href="#"><i className="fa fa-google"></i></a>
-          </li>
-          <li>
-            <a href="#"><i className="fa fa-facebook"></i></a>
-          </li>
-          <li>
-            <a href="#"><i className="fa fa-twitter"></i></a>
-          </li>
-        </ul>
-      </div> -->
     </form>
   </div>
 </template>
@@ -70,7 +51,6 @@ export default {
   },
   methods: {
     async handleButtonClick() {
-      console.log('###', this.$data.email, this.$data.password);
       const { user } = await fb.auth.signInWithEmailAndPassword(
         this.$data.email,
         this.$data.password
@@ -87,13 +67,12 @@ export default {
 
 <style scoped>
 .vue-tempalte {
-  height: 95vh;
+  height: 90vh;
+  background-image: url('../../assets/icons/background.jpg');
+  background-size: 90vh 95vw;
+  padding-top: 30px;
+  padding-bottom: 30px;
 }
-
-/* .navbar-light {
-  background-color: #ffffff;
-  box-shadow: 0px 14px 80px rgba(34, 35, 58, 0.2);
-} */
 
 .inner-block {
   width: 450px;
@@ -116,27 +95,6 @@ h3 {
 label {
   font-weight: 500;
 }
-
-/* .forgot-password,
-.forgot-password a {
-  text-align: right;
-  font-size: 13px;
-  padding-top: 10px;
-  color: #7a7a7a;
-  margin: 0;
-}
-
-.forgot-password a {
-  color: #2554ff;
-} */
-
-/* .social-icons {
-  text-align: center;
-  font-family: 'Open Sans';
-  font-weight: 300;
-  font-size: 1.5em;
-  color: #222222;
-} */
 
 .form-control {
   margin-bottom: 20px;
@@ -185,42 +143,4 @@ label {
   align-items: center;
   justify-content: center;
 }
-
-/* .social-icons ul {
-  list-style: none;
-  margin: 0;
-  padding: 0;
-}
-.social-icons ul li {
-  display: inline-block;
-  zoom: 1;
-  width: 65px;
-  vertical-align: middle;
-  border: 1px solid #e3e8f9;
-  font-size: 15px;
-  height: 40px;
-  line-height: 40px;
-  margin-right: 5px;
-  background: #f4f6ff;
-}
-
-.social-icons ul li a {
-  display: block;
-  font-size: 1.4em;
-  margin: 0 5px;
-  text-decoration: none;
-}
-.social-icons ul li a i {
-  -webkit-transition: all 0.2s ease-in;
-  -moz-transition: all 0.2s ease-in;
-  -o-transition: all 0.2s ease-in;
-  -ms-transition: all 0.2s ease-in;
-  transition: all 0.2s ease-in;
-}
-
-.social-icons ul li a:focus i,
-.social-icons ul li a:active i {
-  transition: none;
-  color: #222222;
-} */
 </style>
